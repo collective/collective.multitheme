1778f5d337322011887b9cfb3e015112ccf231cc
Espen Moe-Nilssen <espen@medialog.no>
2017-12-08 17:13:51 +0100

    Typo in 'typ' for paralax fragment

--- src/collective/multitheme/theme/fragments/richtext_list.py
+++ src/collective/multitheme/theme/fragments/richtext_list.py
@@ -1,4 +1,5 @@
 def get_rt_fields(self):
-    s =  self.data['list_richtext']
-    n = [e.encode('utf-8') for e in s.strip('[]').split(',')]
-    return list(n)
+    #s =  self.data['list_richtext']
+    #n = [e.encode('utf-8') for e in s.strip('[]').split(',')]
+    #return list(n)
+    return self.data['list_richtext']
