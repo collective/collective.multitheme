4a349cef0b14151f7083be4bed052f6bd3f912bb
Espen Moe-Nilssen <espen@medialog.no>
2018-04-26 14:15:06 +0200

    Better images wall (folder view or fragment)

--- src/collective/multitheme/theme/fragments/imageswall.py
+++ src/collective/multitheme/theme/fragments/imageswall.py
@@ -1,4 +1,4 @@
-def get_items(self):
+def get_wallitems(self):
     linked = self.data['linked_folder']
     folder = self.context.portal_catalog(UID=linked)
-    return folder[0].getObject().items()
+    return folder[0].getObject()
