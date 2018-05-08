a756045b9a77486904e0bc04e2d425241632f982
Espen Moe-Nilssen <espen@medialog.no>
2018-05-08 11:26:12 +0200

    relocate URL

--- src/collective/multitheme/theme/fragments/fullpage.py
+++ src/collective/multitheme/theme/fragments/fullpage.py
@@ -2,3 +2,8 @@ def get_wallitems(self):
     linked = self.data['linked_folder']
     folder = self.context.portal_catalog(UID=linked)
     return folder[0].getObject()
+
+def relocate_to(self):
+    linked = self.data['relocate_to']
+    linked_d = self.context.portal_catalog(UID=linked)
+    return linked_d[0].getObject()
