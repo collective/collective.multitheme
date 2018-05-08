0881c25be6ceeb8e678cb07b75d30de2be5bdf92
Espen Moe-Nilssen <espen@medialog.no>
2018-05-08 12:36:55 +0200

    more css style

--- src/collective/multitheme/theme/fragments/fullpage.py
+++ src/collective/multitheme/theme/fragments/fullpage.py
@@ -4,6 +4,5 @@ def get_wallitems(self):
     return folder[0].getObject()
 
 def relocate_to(self):
-    linked = self.data['relocate_to']
-    linked_d = self.context.portal_catalog(UID=linked)
-    return linked_d[0].getObject()
+    url = self.data['relocate_to']
+	return url
