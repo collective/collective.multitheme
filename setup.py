4bbd08d4738699baf1afce0cc0168b3c0d8f110f
T. Kim Nguyen <tkn@alumni.uwaterloo.ca>
2018-05-31 00:23:12 -0500

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.multitheme',
-    version='1.0',
+    version='1.0.1.dev0',
     description="A (Multi) Theme for Plone 5",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
